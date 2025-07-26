d161c2b8b678c9e8ae8bd16a87312371c68e9a2d
man00154 <149796066+man00154@users.noreply.github.com>
2025-07-26 08:55:53 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,3 @@
+# This file makes the 'app' directory a Python package.
+# It can be empty, or it can contain initialization code for the package.
+# For simply allowing imports like 'from app.main import app', an empty file is sufficient.
